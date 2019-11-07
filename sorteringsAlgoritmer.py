1da2e4df29ed77c8bed6bd3fd0cf6e44c5d0ca9d
Skinkeve <Martinhansen4780@gmail.com>
2019-11-07 11:16:56 +0100

    lavede en god del af bubblesort

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,12 +1,37 @@
+#import testcase0
+import matplotlib
 
 def insertionSort():
+    pass
+
+
+
 
 
 def selectionSort():
     pass
 
 def bubbleSort():
-    pass
+    list = ["2","4","3","1"]
+    #listcopy = list.copy()
+    varNumb = 0
+    while 
+        while varNumb < 3:
+            var = list[varNumb]
+            #print(var)
+            nextVar = list[varNumb+1]
+            #extraVar = listcopy[varNumb]
+            #print(nextVar)
+            if var > nextVar:
+                list.pop(varNumb)
+                list.insert(varNumb+1, var)
+                varNumb += 1
+                print(list)
+            else:
+                varNumb += 1
+    print(list)
 
 def mergeSort():
     pass
+
+bubbleSort()
