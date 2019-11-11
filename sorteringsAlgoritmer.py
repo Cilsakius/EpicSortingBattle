a3e1510d886b2f6792d42758dae49eda6056395b
Skinkeve <Martinhansen4780@gmail.com>
2019-11-11 14:25:39 +0100

    færdiggjort bubblesort

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -11,27 +11,37 @@ def insertionSort():
 def selectionSort():
     pass
 
-def bubbleSort():
-    list = ["2","4","3","1"]
+def bubbleSort(ind):
+    list = ind.copy()
     #listcopy = list.copy()
     varNumb = 0
-    while 
-        while varNumb < 3:
-            var = list[varNumb]
-            #print(var)
-            nextVar = list[varNumb+1]
-            #extraVar = listcopy[varNumb]
-            #print(nextVar)
-            if var > nextVar:
-                list.pop(varNumb)
-                list.insert(varNumb+1, var)
-                varNumb += 1
-                print(list)
-            else:
-                varNumb += 1
+    listcheck = 1
+    bool = True
+    length = int(len(list))
+    while bool == True:
+        var = list[varNumb]
+        nextVar = list[varNumb+1]
+        #extraVar = listcopy[varNumb]
+        #print(nextVar)
+        #hvis var er højere end det næste element ryk til højre
+        if var > nextVar:
+            list.pop(varNumb)
+            list.insert(varNumb+1, var)
+            varNumb += 1
+            print(list)
+        else:
+            varNumb += 1
+            print(list)
+
+        if varNumb == length-listcheck:
+            varNumb = 0
+            listcheck += 1
+            print("--------------------")
+            if len(list)-listcheck == 0:
+                bool = False
+
+
     print(list)
 
 def mergeSort():
     pass
-
-bubbleSort()
